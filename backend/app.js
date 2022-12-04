e8c33b62c4d2f00902c9cc81881805b5650d8f41
Roni Kantola <94294699+Kantola-r@users.noreply.github.com>
2022-12-04 18:21:00 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -5,18 +5,18 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var accountRouter = require('./routes/account');
+var cardRouter = require('./routes/card');
 var userRouter = require('./routes/user');
 
 var app = express();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+@@ -18,7 +17,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/account', accountRouter);
+app.use('/card', cardRouter);
 app.use('/user', userRouter);
 
 module.exports = app;
