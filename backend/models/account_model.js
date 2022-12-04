ec8f9f256b217849f1e5230eac641619c4c85f12
Roni Kantola <t9karo00@students.oamk.fi>
2022-12-04 15:25:45 +0200

    otettu account_model.js pois id_account datan lisääminen koska se on auto increment

--- backend/models/account_model.js
+++ backend/models/account_model.js
@@ -9,8 +9,8 @@ const account = {
   },
   add: function(add_data, callback) {
     return db.query(
-      'insert into account (id_account,account_number,balance,bank_name,account_type) values(?,?,?,?,?)',
-      [add_data.id_account, add_data.account_number, add_data.balance, add_data.bank_name, add_data.account_type],
+      'insert into account (account_number,balance,bank_name,account_type) values(?,?,?,?)',
+      [add_data.account_number, add_data.balance, add_data.bank_name, add_data.account_type],
       callback
     );
   },
