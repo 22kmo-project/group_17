b3151240908aa792d63a5159ca41d84cce4fd2ef
Roni Kantola <94294699+Kantola-r@users.noreply.github.com>
2022-12-04 18:24:02 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -10,8 +10,12 @@ var cardRouter = require('./routes/card');
 var userRouter = require('./routes/user');
 
 var app = express();
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-@@ -18,7 +17,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
