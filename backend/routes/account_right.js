6ec90e65db5af9a7cc1b36e7123ff421727928d3
Niko Koivunen <T1koni03@students.oamk.fi>
2022-12-07 18:05:00 +0200

    välitaulukot kunnossa

---
+++ backend/routes/account_right.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const router = express.Router();
+const account_right = require('../models/account_right_model');
+
+router.get('/',
+
+    function (request, response) {
+
+        account_right.getAll(function (err, dbResult) {
+
+            if (err) {
+
+                response.json(err);
+
+            } else {
+
+                console.log(dbResult);
+
+                response.json(dbResult);
+
+            }
+
+        })
+
+    });
+
+
+
+router.get('/:id?',
+
+    function (request, response) {
+
+        account_right.getById(request.params.id, function (err, dbResult) {
+
+            if (err) {
+
+                response.json(err);
+
+            } else {
+
+                response.json(dbResult[0]);
+
+            }
+
+        })
+
+    });
+
+
+router.post('/', 
+function(request, response) {
+  console.log(request.body);
+  account_right.add(request.body, function(err, dbResult) {
+    if (err) {
+      response.json(err);
+    } else {
+      response.json(request.body);
+    }
+  });
+});
+
+
+router.delete('/:id', 
+function(request, response) {
+  account_right.delete(request.params.id, function(err, dbResult) {
+    if (err) {
+      response.json(err);
+    } else {
+      response.json(dbResult);
+    }
+  });
+});
+
+
+router.put('/:id', 
+function(request, response) {
+  account_right.update(request.params.id, request.body, function(err, dbResult) {
+    if (err) {
+      response.json(err);
+    } else {
+      response.json(dbResult);
+    }
+  });
+});
+
+module.exports = router;
