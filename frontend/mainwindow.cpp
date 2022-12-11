affcc81694e41915e3c5096058410a5d0c64d9ce
Roni Kantola <t9karo00@students.oamk.fi>
2022-12-11 20:44:12 +0200

    alettu työstämään debit/credit valintaa ,ei toimi vielä oikein

--- frontend/mainwindow.cpp
+++ frontend/mainwindow.cpp
@@ -14,6 +14,11 @@ MainWindow::MainWindow(QWidget *parent)
     timer = new QTimer(this);
     timer->setSingleShot(true);
     connect(timer, SIGNAL(timeout()), this, SLOT(resetInterface()));
+
+    //Luodaan pääkäyttöliittymään palauttava timer
+        timer2 = new QTimer(this);
+        timer2->setSingleShot(true);
+        connect(timer2, SIGNAL(timeout()), this, SLOT(returnToMainInterface()));
 }
 
 MainWindow::~MainWindow()
@@ -30,6 +35,24 @@ void MainWindow::setUsername(const QString &loggedUser)
 {
     logged_un = loggedUser;
 }
+
+void MainWindow::setSaved_AccountType1(const QString &savedAT1)
+{
+    tallennus_at1 = savedAT1;
+}
+void MainWindow::setSaved_AccountNumber1(const QString &savedAN1)
+{
+    tallennus_an1 = savedAN1;
+}
+void MainWindow::setSaved_AccountType2(const QString &savedAT2)
+{
+    tallennus_at2 = savedAT2;
+}
+void MainWindow::setSaved_AccountNumber2(const QString &savedAN2)
+{
+    tallennus_an2 = savedAN2;
+}
+
 
 //Aikakatkaisuun liittyvä, käyttöliittymän resetointi
 void MainWindow::resetInterface()
@@ -126,6 +149,7 @@ void MainWindow::loginSlot(QNetworkReply *reply)
 
                 // Lähdetään tarkistamaan onko useampi kuin yksi tili kortissa
                 fetchUserID();
+
                 }
             }
         }
@@ -162,6 +186,24 @@ void MainWindow::fetchUserIDSlot(QNetworkReply *reply)
 
     //Kutsutaan funktio jonka avulla asetetaan haettu id_user sekä selvitetään kytkettyjen tilien määrä
     fetchAccounts();
+    //haetaan myös tähän väliin sisään kirjautuvan henkilön nimi
+    fetch_user_full_name();
+
+    QString testi = "3";
+    QString testi2= "1";
+
+    fetch_DataForCompare(testi);
+    //asetetaan tilintyyppi ja -numero talteen
+    setSaved_AccountType1(test_at1), setSaved_AccountNumber1(test_an1);
+
+    qDebug()<<"tilintyyppi1:"+tallennus_at1+" ja tilinumero:"+tallennus_an1;
+
+    fetch_DataForCompare(testi2);
+    //asetetaan toinen tilityyppi ja -numero talteen
+    setSaved_AccountType2(tallennus_at2), setSaved_AccountNumber2(tallennus_an2);
+
+    qDebug()<<"tilintyyppi1.1:"+tallennus_at1+" ja tilinumero:"+tallennus_an1;
+    qDebug()<<"tilintyyppi3:"+tallennus_at2+" ja tilinumero:"+tallennus_an2;
 
     reply->deleteLater();
     userIdManager->deleteLater();
@@ -213,3 +255,200 @@ void MainWindow::fetchAccountSlot(QNetworkReply *reply)
      reply->deleteLater();
      fetchAccManager->deleteLater();
 }
+
+//tästä alkaa Ronin tekemät osat ---------------------------------------------------------------------------------------------------------------------------
+
+//aikakatkaisuun liittyvä, pääkäyttöliittymään palautus
+void MainWindow::returnToMainInterface()
+{
+    qDebug()<<"Return Timer Finished.";
+    ui->stackedWidget->setCurrentIndex(4);
+    //aloitetaan myös uusi 30sekunnin timer joka palauttaa käyttäjän aloitusnäytöön
+    timer->start(30000);
+    qDebug()<<"Timer Started.";
+}
+    //haetaan käyttäjän kokonimi
+void MainWindow::fetch_user_full_name()
+{
+    QString site_url=MyUrl::getBaseUrl()+"/user/"+userid;
+    QNetworkRequest request((site_url));
+    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
+    //WEBTOKEN ALKU
+        request.setRawHeader(QByteArray("Authorization"),(mytn));
+    //WEBTOKEN LOPPU
+    fetchUserFullNameManager = new QNetworkAccessManager(this);
+    connect(fetchUserFullNameManager, SIGNAL(finished (QNetworkReply*)), this, SLOT(user_full_nameSlot(QNetworkReply*)));
+
+    reply = fetchUserFullNameManager->get(request);
+}
+    //tallennetaan käyttäjän kokonimi kahteen eri muuttujaan, koska user tablessa se on eritelty user_fname, user_lname
+void MainWindow::user_full_nameSlot (QNetworkReply *reply)
+{
+
+ response_user_fullname=reply->readAll();
+ QJsonDocument json_doc = QJsonDocument::fromJson(response_user_fullname);
+ QJsonObject json_obj = json_doc.object();
+
+    fname=json_obj["user_fname"].toString();
+    lname=json_obj["user_lname"].toString();
+    qDebug()<<"käyttäjän nimi:"+fname+" "+lname;
+
+    ui->current_user_label_1->setText(fname+ " "+ lname);
+    ui->current_user_label_2->setText(fname+ " "+ lname);
+
+    reply->deleteLater();
+    fetchUserFullNameManager->deleteLater();
+ }
+
+    //haetaan kortin tyyppi ja numero vertailua varten
+void MainWindow::fetch_DataForCompare(QString id)
+{
+    QString site_url=MyUrl::getBaseUrl()+"/account/"+id;
+    QNetworkRequest request((site_url));
+    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
+    //WEBTOKEN ALKU
+        request.setRawHeader(QByteArray("Authorization"),(mytn));
+    //WEBTOKEN LOPPU
+    CompareDataManager = new QNetworkAccessManager(this);
+    connect(CompareDataManager, SIGNAL(finished (QNetworkReply*)), this, SLOT(CompareDataSlot(QNetworkReply*)));
+
+    reply = CompareDataManager->get(request);
+}
+//tallennetaan testiarvot vertailua varten muuttujiin
+void MainWindow::CompareDataSlot (QNetworkReply *reply)
+{
+
+    response_CompareData=reply->readAll();
+    QJsonDocument json_doc = QJsonDocument::fromJson(response_CompareData);
+    QJsonObject json_obj = json_doc.object();
+
+    test_at1=json_obj["account_type"].toString();
+    test_an1=json_obj["account_number"].toString();
+
+    qDebug()<<"tilintyyppi:"+test_at1+" ja tilinumero:"+test_an1;
+
+    reply->deleteLater();
+    CompareDataManager->deleteLater();
+
+}
+
+    //testataan onko tilin tyyppi credit vai debit
+    QString MainWindow::CreditOrDebit_testing(QString a)
+
+    {
+
+       if(a==debit)
+       {
+           current_account_number=test_an1;
+       }
+
+       else
+       {
+           current_account_number=test_an2;
+       }
+       qDebug()<<"tämänhetkinen tilinumero on:"+current_account_number;
+        return a;
+
+    }
+
+
+
+    //haetaan tilitiedot
+void MainWindow::fetch_account_information()
+{
+    QString site_url=MyUrl::getBaseUrl()+"/account_information/"+current_account_number;
+    QNetworkRequest request((site_url));
+    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
+    //WEBTOKEN ALKU
+        request.setRawHeader(QByteArray("Authorization"),(mytn));
+    //WEBTOKEN LOPPU
+    account_informationManager = new QNetworkAccessManager(this);
+    connect(account_informationManager, SIGNAL(finished (QNetworkReply*)), this, SLOT(account_informationSlot(QNetworkReply*)));
+
+    reply = account_informationManager->get(request);
+}
+    //tallennetaan tilitiedot muuttujiin
+void MainWindow::account_informationSlot (QNetworkReply *reply)
+{
+
+ response_account_information=reply->readAll();
+ QJsonDocument json_doc = QJsonDocument::fromJson(response_account_information);
+ QJsonObject json_obj = json_doc.object();
+
+    bank_name=json_obj["bank_name"].toString();
+    account_holder=json_obj["account_holder"].toString();
+    account_type=json_obj["account_type"].toString();
+    balance=QString::number(json_obj["balance"].toDouble());
+    account_number=json_obj["account_number"].toString();
+    account_right=json_obj["account_right"].toString();
+
+    //testausta
+
+    qDebug()<<"pankin nimi:"+bank_name;
+    qDebug()<<"tilin omistaja:"+account_holder;
+    qDebug()<<"tilityyppi:"+account_type;
+    qDebug()<<"tilin saldo:"+balance;
+    qDebug()<<"tilinumero:"+account_number;
+    qDebug()<<"käyttöoikeus:"+account_right;
+
+    reply->deleteLater();
+    account_informationManager->deleteLater();
+ }
+
+
+
+
+
+// tästä alaspäin nappien toimintoja
+void MainWindow::on_debit_button_clicked()
+{
+    CreditOrDebit_testing(debit);
+
+    ui->stackedWidget->setCurrentIndex(4);
+    timer->start(30000);
+    qDebug()<<"Timer Started.";
+}
+
+void MainWindow::on_credit_button_clicked()
+{
+    CreditOrDebit_testing(credit);
+
+    ui->stackedWidget->setCurrentIndex(4);
+    timer->start(30000);
+    qDebug()<<"Timer Started.";
+}
+
+void MainWindow::on_withdraw_button_clicked()
+{
+    timer->stop();
+    fetch_account_information();
+    ui->stackedWidget->setCurrentIndex(5);
+    timer2->start(10000);
+    qDebug()<<"Return Timer Started.";
+}
+
+void MainWindow::on_balance_button_clicked()
+{
+    timer->stop();
+    fetch_account_information();
+    ui->stackedWidget->setCurrentIndex(8);
+    timer2->start(10000);
+    qDebug()<<"Return Timer Started.";
+}
+
+void MainWindow::on_transactions_button_clicked()
+{
+    timer->stop();
+    fetch_account_information();
+    ui->stackedWidget->setCurrentIndex(9);
+    timer2->start(10000);
+    qDebug()<<"Return Timer Started.";
+}
+
+void MainWindow::on_log_out_button_clicked()
+{
+    timer->stop();
+    ui->stackedWidget->setCurrentIndex(0);
+}
+
+//tähän lopppuu Ronin tekemät osat -------------------------------------------------------------------------------------------------
--- frontend/mainwindow.h
+++ frontend/mainwindow.h
@@ -27,12 +27,21 @@ public:
     QString userid;
     QByteArray mytn;
     QString logged_un;
+    QString bank_name, account_holder,account_type, account_number, account_right, balance, fname, lname;
+    QString tallennus_at1, tallennus_an1, tallennus_at2, tallennus_an2;
+    QString current_account_number;
 
     void setWebToken(const QByteArray &newWebToken);
     void setUsername(const QString &loggedUser);
 
+    void setSaved_AccountType1(const QString &savedAT1);
+    void setSaved_AccountNumber1(const QString &savedAN1);
+    void setSaved_AccountType2(const QString &savedAT2);
+    void setSaved_AccountNumber2(const QString &savedAN2);
+
 public slots:
     void resetInterface();
+    void returnToMainInterface();
 
 private slots:
     void on_begin_button_clicked();
@@ -43,20 +52,43 @@ private slots:
     void fetchAccounts();
     void fetchAccountSlot(QNetworkReply *reply);
 
+
+    void on_debit_button_clicked();
+    void on_credit_button_clicked();
+    void fetch_user_full_name();
+    void user_full_nameSlot(QNetworkReply *reply);
+    void fetch_account_information();
+    void account_informationSlot (QNetworkReply *reply);
+    void fetch_DataForCompare(QString);
+    void CompareDataSlot (QNetworkReply *reply);
+    QString CreditOrDebit_testing(QString);
+    void on_withdraw_button_clicked();
+    void on_balance_button_clicked();
+    void on_transactions_button_clicked();
+    void on_log_out_button_clicked();
+
 private:
     Ui::MainWindow *ui;
     QNetworkAccessManager *loginManager;
     QNetworkAccessManager *userIdManager;
     QNetworkAccessManager *fetchAccManager;
+    QNetworkAccessManager *account_informationManager;
+    QNetworkAccessManager *fetchUserFullNameManager;
+    QNetworkAccessManager *CompareDataManager;
     QNetworkReply *reply;
     QByteArray login_data;
     QByteArray fetch_acc_data;
     QByteArray fetch_user_data;
+    QByteArray response_account_information;
+    QByteArray response_user_fullname;
+    QByteArray response_CompareData;
     QString username;
     QString pin_code;
     QString warning1 = "PALVELIN EI VASTAA. YRITÄ MYÖHEMMIN UUDELLEEN.";
     QString warning2 = "VIRHE TIETOKANTA YHTEYDESSÄ. YRITÄ MYÖHEMMIN UUDELLEEN.";
     QString warning3 = "VIRHEELLINEN KÄYTTÄJÄTUNNUS TAI PIN-KOODI! YRITÄ UUDELLEEN. \nYRITYSKERTOJA JÄLJELLÄ: ";
     QTimer *timer;
+    QTimer *timer2;
+    QString test_at1, test_at2, test_an1, test_an2,debit, credit;
 };
 #endif // MAINWINDOW_H
